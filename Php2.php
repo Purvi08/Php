a9de245a3c8f3f39db750899526d9297bf5b9ab0
Purvi Rastogi <80806656+Purvi08@users.noreply.github.com>
2021-05-15 22:26:46 +0530

    Update Php2.php

--- Php2.php
+++ Php2.php
@@ -1,3 +1,9 @@
+<!-- a)	Write a PHP Calculator class which will accept two values as arguments, then add them, subtract them, multiply them together, or divide them on request. For example :
+$mycalc = new MyCalculator( 12, 6);
+echo $mycalc- > add(); // Displays 18
+echo $mycalc- > multiply(); // Displays 72  -->
+
+
 class Calculator{
 
     public $num1,$num2;
